db8738700e80f27194f7c18544f1e34420859340
root <root@debian-dev-1>
2020-01-05 22:53:09 +0000

    Feat: removed data methods that have been moved and upgraded in data_methods.py.

--- cdf.py
+++ cdf.py
@@ -19,25 +19,3 @@ def csv_to_df(file='olympics.csv', skip=4):
         print('none')
         return None
 
-
-def update_using_iterrows(target_df, source_df):
-    # df1 = dataframe to update
-    # source_df = dataframe with source values
-    for source_df_index, source_df_row in source_df.iterrows():
-        for target_df_index, target_df_row in target_df.iterrows():
-            if target_df_row['Edition'] == source_df_row['Edition']:
-                if  not target_df_row['Athlete'] and not target_df_row['Sport']:
-                    athlete = source_df_row['Athlete']
-                    sport = source_df_row['Sport']
-    
-                    target_df.at[target_df_index, 'Athlete'] = athlete
-                    target_df.at[target_df_index, 'Sport'] = sport
-
-
-def update_using_apply(target_df, source_df):
-    # Update values in target_df with values of 
-    # source_df
-    def _update_values(df):
-        pass
-
-
