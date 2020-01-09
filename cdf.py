bb910a27551e2003ec5ce59ed00f26bd4935aecc
root <root@debian-dev-1>
2020-01-09 01:15:09 +0000

    Feat: added small function timeit that takas a string expression, times its evaluation and returns the return value of the evaluation, while sending the seconds with 3 diigts to stdout. Can be extended to support taking a function or made a decorator

--- cdf.py
+++ cdf.py
@@ -2,6 +2,7 @@
 Module for converting csv files to dataframes
 """
 import os
+import time
 from glob import iglob
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,8 +10,10 @@ import matplotlib as mpl
 
 
 def csv_to_df(file='olympics.csv', skip=4):
-    # Get sthe file to pass to pd.read_csv
-    # and rerturns a pd.DataFrame of that file
+    """
+    Get sthe file to pass to pd.read_csv
+    and rerturns a pd.DataFrame of that file
+    """
     files = [i for i in iglob('**/*', recursive=True) if file in i]
     ocsv = files[0] if files else None
     print('ocsv: %s' % ocsv)
@@ -23,13 +26,27 @@ def csv_to_df(file='olympics.csv', skip=4):
 
 
 def save_plot(pandas_series, output_path='./my_plot.png'):
-    # takes a pandas.Series, plots it,
-    # and saves a png of the plot
-    # If the series has a pandas.Series.name, it will print
-    # Returns the fig object
+    """
+    takes a pandas.Series, plots it,
+    and saves a png of the plot
+    If the series has a pandas.Series.name, it will print
+    Returns the fig object
+    """
     fig = plt.figure()
     _ = fig.add_subplot(pandas_series.plot())
     _ = fig.savefig(output_path)
     print('pandas.Series.name of "%s" plotted and saved to "%s"' % (pandas_series.name, output_path))
     return fig
+
+
+def timeit(expr):
+    """
+    evaluates an expression <str> and returns the output of the expression
+    sends the time in seconds as a <str> to stdout
+    """
+    start = time.time()
+    result = eval(expr)
+    total = time.time() - start
+    print('%.3f seconds' % total)
+    return result
 
